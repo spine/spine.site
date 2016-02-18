30124a05d227e5055e4cc47aa1939434e973b3e5
aeischeid <a.eischeid@gmail.com>
2016-02-18 15:40:45 -0600

    fix typo in download link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def download
-    download_url = "https://github.com/spine/spine/archive/v#{version}.zip"
+    download_url = "https://github.com/spine/spine/archive/v.#{version}.zip"
     redirect_to download_url
   end
 end
